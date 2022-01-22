e319fcc671f1f321e14afae16c4ad6d20dea0846
SpotMatch <76890200+SpotMatch@users.noreply.github.com>
2022-01-22 13:30:52 +0100

    update

--- buttonstatus/buttonstatus.js
+++ buttonstatus/buttonstatus.js
@@ -2,3 +2,9 @@
 
 $( "#mydiv" ).text( "Hallo Welt in DIV" )
 $( "#mytext" ).text( "Hallo Welt in P" )
+
+let testdiv = document.getElementById("mydiv")
+let testtext = document.getElementById("mytext")
+
+testdiv.innerHTML("JS to DIV")
+testtext.innerHTML("JS to TEXT")
